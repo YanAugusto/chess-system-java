9e91eccdeb8ee8002ec0653e112430672a71f9ab
Yan Augusto <marcoyan11@hotmail.com>
2021-06-30 14:14:57 -0300

    ChessException

--- src/boardgame/BoardException.java
+++ src/boardgame/BoardException.java
@@ -4,7 +4,8 @@ public class BoardException extends RuntimeException {
 	
 	private static final long serialVersionUID = 1L;
 	
-	public BoardException(String msg) {
+	public BoardException(String msg)
+	{
 		super(msg);
 	}
 	
